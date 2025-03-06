17d8357de382bf3cbcba548281f86287a842c0af
Eroasulyan <eroasulyan2@gmail.com>
2025-03-06 15:44:22 +0400

    my new commit

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-let nav = document.querySelector('#nav')
-let navBtn = document.querySelector('#nav-btn')
-let navBtnimg = document.querySelector('#nav-btn-img')
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./imge/nav-close.svg";
+    } else {
+        navBtnImg.src = './imge/nav-open.svg';
+    }
+}
 
 
-navBtn.addEventListener("click", () => {
-    nav.classList.toggle("open");
-});
